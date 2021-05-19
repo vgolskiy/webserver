b9f7c422be1ec3ae62284cb5a1e25cb6ae7d993e
Card <cwindom@ox-g1.msk.21-school.ru>
2021-05-19 23:02:19 +0300

    s

--- Response.cpp
+++ Response.cpp
@@ -4,7 +4,8 @@ Response::Response(Client *client, t_server *server, std::string loc, std::strin
 	_response = "";
 	_method = client->get_request()->get_method();
 	_body = "";
-	_loc = get_location(server, loc); 
+	_loc = get_location(server, loc);
+	_status = client->get_request()->get_status();
 }
 
 Response::~Response() {}
@@ -136,69 +137,21 @@ void Response::create_response(void) {
 
 void Response::set_status()
 {
-	_status[100] = "Continue";
-	_status[101] = "Switching Protocols";
-	_status[102] = "Processing";
-	_status[200] = "OK";
-	_status[201] = "Created";
+	_status[200] = "OK"; //get head post
+	_status[201] = "Created"; //put
 	_status[202] = "Accepted";
-	_status[203] = "Non-authoritative Information";
-	_status[204] = "No Content";
-	_status[205] = "Reset Content";
-	_status[206] = "Partial Content";
-	_status[207] = "Multi-Status";
-	_status[208] = "Already Reported";
-	_status[226] = "IM Used";
-	_status[300] = "Multiple Choices";
-	_status[301] = "Moved Permanently";
-	_status[302] = "Found";
-	_status[303] = "See Other";
-	_status[304] = "Not Modified";
-	_status[305] = "Use Proxy";
-	_status[307] = "Temporary Redirect";
-	_status[308] = "Permanent Redirect";
+	_status[204] = "No Content"; //нет содержимого для put
+	_status[301] = "Moved Permanently"; //если uri изменен
+	_status[302] = "Found"; //get или head + в ответе будет новый локейшн
+	_status[304] = "Not Modified"; //используется для кеширования
 	_status[400] =  "Bad Request";
-	_status[401] =  "Unauthorized";
-	_status[402] =  "Payment Required";
+	_status[401] =  "Unauthorized"; //нужна аутентификация
 	_status[403] =  "Forbidden";
 	_status[404] =  "Not Found";
-	_status[405] =  "Method Not Allowed";
-	_status[406] =  "Not Acceptable";
-	_status[407] =  "Proxy Authentication Required";
-	_status[408] =  "Request Timeout";
-	_status[409] =  "Conflict";
-	_status[410] =  "Gone";
-	_status[411] =  "Length Required";
-	_status[412] =  "Precondition Failed";
-	_status[413] =  "Payload Too Large";
-	_status[414] =  "Request-URI Too Long";
-	_status[415] =  "Unsupported Media Type";
-	_status[416] =  "Requested Range Not Satisfiable";
-	_status[417] =  "Expectation Failed";
-	_status[418] =  "I'm a teapot";
-	_status[421] =  "Misdirected Request";
-	_status[422] =  "Unprocessable Entity";
-	_status[423] =  "Locked";
-	_status[424] =  "Failed Dependency";
-	_status[426] =  "Upgrade Required";
-	_status[428] =  "Precondition Required";
-	_status[429] =  "Too Many Requests";
-	_status[431] =  "Request Header Fields Too Large";
-	_status[444] =  "Connection Closed Without Response";
-	_status[451] =  "Unavailable For Legal Reasons";
-	_status[499] =  "Client Closed Request";
-	_status[500] =  "Internal Server Error";
-	_status[501] =  "Not Implemented";
+	_status[405] =  "Method Not Allowed"; //не возвращают этот код head и get
+	_status[500] =  "Internal Server Error"; //внутренняя ошибка сервера
+	_status[501] =  "Not Implemented"; //не возвращают этот код head и get
 	_status[502] =  "Bad Gateway";
-	_status[503] =  "Service Unavailable";
-	_status[504] =  "Gateway Timeout";
-	_status[505] =  "HTTP Version Not Supported";
-	_status[506] =  "Variant Also Negotiates";
-	_status[507] =  "Insufficient Storage";
-	_status[508] =  "Loop Detected";
-	_status[510] =  "Not Extended";
-	_status[518] =  "I am a teapot";
-	_status[511] =  "Network Authentication Required";
-	_status[599] =  "Network Connect Timeout Error";
+	_status[503] =  "Service Unavailable"; //отключен или перегружен, не готов обрабатывать запрос
 }
 
